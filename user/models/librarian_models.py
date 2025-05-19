62e04491e993ad21f203b8771c5072f42d8fde8e
amit <amit210905@gmail.com>
2025-05-19 18:39:15 +0600

    Ref: user representation changed.

--- user/models/librarian_models.py
+++ user/models/librarian_models.py
@@ -4,6 +4,10 @@ from user.models import User
 
 
 class Librarian(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='librarians')
     start_date = models.DateField()
     end_date = models.DateField()
+
+
+    def __str__(self):
+        return str(self.user)
---
+++ user/serializers/teacher_serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from user.models import Teacher
+
+class TeacherSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Teacher
+        fields = '__all__'
