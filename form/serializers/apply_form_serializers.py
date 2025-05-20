4afe4ef05176d56b65f72a5b3391598acddcc2c5
amit <amit210905@gmail.com>
2025-05-21 01:58:05 +0600

    Ref: API's approvals and form list based on user.

--- form/serializers/apply_form_serializers.py
+++ form/serializers/apply_form_serializers.py
@@ -17,3 +17,35 @@ class ApplyFormSerializer(serializers.ModelSerializer):
             'id', 'is_paid', 'is_controller_approved', 'is_dsa_approved', 'is_librarian_approved',
             'is_head_approved', 'head_approved_by', 'librarian_approved_by', 'dsa_approved_by'
         )
+
+
+
+class HeadApproveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApplyForm
+        fields = '__all__'
+        read_only_fields = (
+            'id', 'is_paid', 'is_controller_approved', 'is_dsa_approved', 'is_librarian_approved', 'librarian_approved_by',
+            'dsa_approved_by', 'degree', 'cgpa', 'total_credit', 'earned_credit', 'date_of_last_exam', 'student', 'discipline', 'hall'
+        )
+
+
+
+class DsaApproveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApplyForm
+        fields = '__all__'
+        read_only_fields = (
+            'id', 'is_paid', 'is_controller_approved', 'is_head_approved', 'is_librarian_approved', 'librarian_approved_by',
+            'head_approved_by', 'degree', 'cgpa', 'total_credit', 'earned_credit', 'date_of_last_exam', 'student', 'discipline', 'hall'
+        )
+
+
+class LibrarianApproveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApplyForm
+        fields = '__all__'
+        read_only_fields = (
+            'id', 'is_paid', 'is_controller_approved', 'is_head_approved', 'is_dsa_approved', 'dsa_approved_by',
+            'head_approved_by', 'degree', 'cgpa', 'total_credit', 'earned_credit', 'date_of_last_exam', 'student', 'discipline', 'hall'
+        )
