47c681adadfca3eb61299745368b96ba05ff3abd
amit <amit210905@gmail.com>
2025-05-21 13:37:05 +0600

    Ref: provostapproval api.

--- form/urls.py
+++ form/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from form.views.apply_form_views import ApplyFormListCreateView, HeadFormRetrieveUpdateDestroyView, DsaFormRetrieveUpdateDestroyView, LibrarianFormRetrieveUpdateDestroyView
+from form.views.apply_form_views import ApplyFormListCreateView, HeadFormRetrieveUpdateDestroyView, DsaFormRetrieveUpdateDestroyView, LibrarianFormRetrieveUpdateDestroyView, ProvostFormRetrieveUpdateDestroyView
 
 urlpatterns = [
     path("", ApplyFormListCreateView.as_view(), name='form_list_create'),
     path("/head/<str:id>", HeadFormRetrieveUpdateDestroyView.as_view(), name='form_update_head'),
     path("/dsa/<str:id>", DsaFormRetrieveUpdateDestroyView.as_view(), name='form_update_dsa'),
     path("/librarian/<str:id>", LibrarianFormRetrieveUpdateDestroyView.as_view(), name='form_update_librarian'),
+    path("/provost/<str:id>", LibrarianFormRetrieveUpdateDestroyView.as_view(), name='form_update_librarian'),
     # path("/<str:id>", HallDetailAPIView.as_view(), name='hall_details'),
 ]
